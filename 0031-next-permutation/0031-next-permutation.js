fdaa25191b0dc61605d3fed99e1d73eaece25a7c
Ashutosh Tiwari <97720235+InCoderWeb@users.noreply.github.com>
2025-03-20 13:46:27 +0530

    Time: 1 ms (23.63%), Space: 54 MB (42.3%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    let ind = -1, n = nums.length;
+    for(let i = n - 2; i >= 0; i--) {
+        if(nums[i] < nums[i + 1]) {
+            ind = i
+            break;
+        }
+    }
+
+    if(ind == -1) {
+        nums.reverse()
+        return;
+    }
+
+    for(let i = n - 1; i > ind; i--) {
+        if(nums[i] > nums[ind]) {
+            [nums[i], nums[ind]] = [nums[ind], nums[i]]
+            break;
+        }
+    }
+
+    nums.splice(ind + 1, n - ind - 1, ...nums.slice(ind + 1).reverse());
+};
